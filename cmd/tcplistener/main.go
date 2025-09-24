f482a91f369bc38ebc3ae94e8a0a7880f94820f6
AbhishekPSingh07 <abhisheksinghas1205@gmail.com>
2025-09-24 23:47:12 +0530

    added request parser into tcp listener'

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,45 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
+	request "httpServerGo/internal"
 	"log"
 	"net"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-
-	}()
-
-	return out
-}
 func main() {
 
 	listener, err := net.Listen("tcp", ":42069")
@@ -53,10 +20,15 @@ func main() {
 			log.Fatal("error", err)
 		}
 
-		lines := getLinesChannel(conn)
-		for line := range lines {
-			fmt.Printf("read: %s\n", line)
+		request, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", err)
 		}
+
+		fmt.Printf("Request Line\n")
+		fmt.Printf("- Method: %s\n", request.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", request.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", request.RequestLine.HttpVersion)
 	}
 
 }
